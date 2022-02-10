fbae60388a4dff532c5aa257d9f35ba9ed8a0f7f
Phil-FH <2110781019@fh-burgenland.at>
2022-02-10 22:16:02 +0100

    proxy angepasst ssl certbot wird erstellt und funktioniert jetzt

--- modules/podtatohead/outputs.tf
+++ modules/podtatohead/outputs.tf
@@ -2,5 +2,5 @@ output "podtato-elb-url" {
   value = "http://${aws_elb.main_elb.dns_name}"
 }
 output "podtato-proxy-url" {
-  value = "https://${aws_instance.proxy.public_ip}"
+  value = "https://${aws_instance.proxy.public_ip}.nip.io"
 }
